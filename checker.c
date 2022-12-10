912f9c78dc7a0351ba210acb0e3c12be9ad051f8
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-12-10 18:47:25 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -20,7 +20,7 @@ int CheckBatChargeRate(float chargeRate)
     if(chargeRate > 0.8){
     printf("Charge Rate out of range!\n");
     return 0;}
-    return 1;
+   return 1;
 }
 int batteryIsOk(float temperature,float soc,float chargeRate)
 {
